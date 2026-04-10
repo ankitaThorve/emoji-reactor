56aac04cc7a0cdae0de19e8deb6c882b6f2cbf64
Ankita <ankietathorve@gmail.com>
2026-04-10 21:13:11 +0530

    Update function to count click numbers

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const happyBtn = document.querySelector("#happy-btn");
-happyBtn.addEventListener("click", () => {
-  console.log("Button clicked!");
+function updateCount(happyBtn) {
+  // console.log("Button clicked!");
   const countElement = happyBtn.querySelector(".count");
-  const text = countElement.textContent;
-  let currCount = Number(text.split("/")[0]);
+  let currCount = +countElement.textContent.split("/")[0];
   // console.log("Current count:", currCount);
   if (currCount === 10) return;
   currCount++;
   countElement.textContent = `${currCount}/10`;
+}
+
+const happyBtn = document.querySelector("#happy-btn");
+
+happyBtn.addEventListener("click", () => {
+  updateCount(happyBtn);
 });
