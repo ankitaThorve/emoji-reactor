513c8e10b5a9b0181087e11eaf3e9f169ed7eec7
Ankita <ankietathorve@gmail.com>
2026-04-11 11:36:26 +0530

    Add event listeners to other emoji buttons

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ const happyBtn = document.querySelector("#happy-btn");
 happyBtn.addEventListener("click", () => {
   updateCount(happyBtn);
 });
+
+const confusedBtn = document.querySelector("#confused-btn");
+
+confusedBtn.addEventListener("click", () => {
+  updateCount(confusedBtn);
+});
+
+const sadBtn = document.querySelector("#sad-btn");
+
+sadBtn.addEventListener("click", () => {
+  updateCount(sadBtn);
+});
+
+const lovingBtn = document.querySelector("#loving-btn");
+
+lovingBtn.addEventListener("click", () => {
+  updateCount(lovingBtn);
+});
+
+
+
